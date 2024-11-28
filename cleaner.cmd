a6db257ee4464f822cf1eeb1745ed9179ca4852c
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2024-11-28 16:07:37 +0900

    Add files via upload

---
+++ cleaner.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+del %userprofile%\AppData\Roaming\Microsoft\Windows\Recent\AutomaticDestinations\*
+del %userprofile%\AppData\Roaming\Microsoft\Windows\Recent\CustomDestinations\*
+del C:\Windows\Temp\*
+
+::pause
